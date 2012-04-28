c923f53d39a6594b5c241a3ba2402f676cf1e965
Jack Moggach <jack.au@gmail.com>
2012-04-28 17:15:42 +1000

    Added media section

--- js/main.js
+++ js/main.js
@@ -12,12 +12,14 @@ var helper = {
 	getElems: function() {
 		playout.$elem = $('#play-history');
 		hottest100.$elem = $('#hottest100');
+		media.$elem = $('#media');
 	},
 	
 	update: function() {
 		helper.artist = playout.getCurrentArtist();
 		helper.track = playout.getCurrentTrack();
 		hottest100.update(helper.artist);
+		media.update(helper.artist);
 	}
 	
 };
---
+++ js/media.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/* Media namespace */
+
+var media = {
+	baseurl: 'http://triplejgizmo.abc.net.au/hottest100/json/media/index.php',
+	
+	update: function(artist) {
+		var query = media.baseurl + '?artist=' + artist;
+		$.ajax({
+			dataType: 'jsonp',
+			url: query,
+			success: function(data) {
+				if(data.length>0) {
+					$(data).each(function() {
+						var url = this.url;
+						var title = this.title;
+						var description = this.description;
+						var date = this.date;
+						var year = this.year;
+						
+						var $title = $('<p/>').addClass('title').append($('<a/>').text(title).attr('href',url).attr('target','_blank'));
+						var $date = $('<p/>').addClass('date').text(date);
+						var $description = $('<p/>').addClass('description').text(description);
+						var $li = $('<li/>').append($title,$date,$description);
+						media.$elem.append($li);
+					});
+				} else {
+					media.$elem.append($('<p/>').html('No results'));
+				}
+			},
+			error: function() {
+				console.log('error!');
+			}
+		});
+	}
+
+}
