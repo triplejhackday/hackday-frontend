b39f5aed474e76a49eb99f12b5fe51acf2a02a20
Jack Moggach <jack.au@gmail.com>
2012-05-02 18:31:02 +1000

    Added bolding around query

--- json/autocomplete.php
+++ json/autocomplete.php
@@ -8,15 +8,23 @@ $q = @$_GET['q'];
 $callback = @$_GET['callback'];
 
 if(strlen($q)>2) {
-	$q = "%" . $q . "%";
+	$term = "%" . $q . "%";
 	$db = new PDO('sqlite:../../hackday-backend/plays.db');
 	
 	$query = "SELECT DISTINCT artist FROM unearthed WHERE artist like :artist LIMIT 5";
-	$params = array(':artist'=>$q);
+	$params = array(':artist'=>$term);
 	$sth = $db->prepare($query);
 	$sth->execute($params);
 	$output = $sth->fetchAll(PDO::FETCH_COLUMN);
-	
+	foreach($output as &$item) {
+		$item = wrapTerm($item,$q);
+	}
 	outputJson($output,$callback);
+}
+
+function wrapTerm($s,$q) {
+	$length = strlen($q);
+	$pos = stripos($s,$q);
+	return substr($s,0,$pos) . "<b>" . substr($s,$pos,$length) . "</b>" . substr($s,$pos + $length);
 }
 ?>
